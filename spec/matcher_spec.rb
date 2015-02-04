561324e6f1e445484fc621c310321dfef199cc82
Toth Peter <tothpeter08@gmail.com>
2015-02-04 21:28:07 +0100

    2.3-Matchers (Part 2)

--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -31,10 +31,64 @@ describe "Matchers" do
     # expect(object).to be_an_instance_of Numeric
   end
 
-  it "asserts on truthiness"
-  it "asserts errors"
-  it "asserts throws"
-  it "asserts on predicates"
-  it "asserts on collections"
-  it "asserts asserts"
+  it "asserts on truthiness" do
+    bool = true
+    falsy_bool = false
+    nil_value = nil
+    object = Class.new
+
+    expect(bool).to be true
+    expect(falsy_bool).to be false
+    expect(nil_value).to be nil
+    expect(nil_value).to be_falsy
+    expect(object).to be_truthy
+  end
+
+  it "asserts errors" do
+    expect do
+      raise TypeError
+    end.to raise_error TypeError
+
+    expect do
+      raise ArgumentError
+    end.to raise_error
+  end
+
+  it "asserts throws" do
+    expect {
+      throw :oops
+    }.to throw_symbol :oops
+
+    expect {
+      throw :hooray
+    }.to throw_symbol
+  end
+
+  it "asserts on predicates" do
+    class A
+      def good?
+        true
+      end
+    end
+
+    expect(A.new).to be_good
+  end
+
+  it "asserts on collections" do
+    list = [
+      :one,
+      :two,
+      :three,
+      :four
+    ]
+
+    expect(list).to include :four
+    expect(list).to start_with :one
+    expect(list).to start_with [ :one, :two ]
+    expect(list).to end_with :four
+  end
+
+  it "negates asserts" do
+    expect(3).not_to be 5
+  end
 end
